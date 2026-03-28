1430b505f8492bf04bd394a0bb0502e73eb107c9
Lauren Brodsky <158362902+Laurenb1208@users.noreply.github.com>
2026-03-28 16:22:21 -0500

    initial flask app

---
+++ app.py
@@ -0,0 +1,14 @@
+
+# module 10 - Flask Application
+# Lauren Brodsky 3/28/2026
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!!"
+
+if __name__ == "__main__":
+      app.run(host="0.0.0.0", port=5002, debug=True)
