a9dd3fbfe405935e8bd6aaf10159afded3b56668
Lauren Brodsky <158362902+Laurenb1208@users.noreply.github.com>
2026-03-28 16:54:24 -0500

    added templates and styling

--- app.py
+++ app.py
@@ -2,13 +2,12 @@
 # module 10 - Flask Application
 # Lauren Brodsky 3/28/2026
 
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
       app.run(host="0.0.0.0", port=5002, debug=True)
